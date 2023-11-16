54ade51e033f11cea16dbda382dc825dccade9aa
stanislav553 <stanislav.gutnov@yandex.ru>
2023-11-16 21:56:58 +0300

    write 3 section

---
+++ js/main.js
@@ -0,0 +1,24 @@
+gsap.to('.offer__title-line, .offer__sun, .offer__lightnings', {
+  rotate: 0,
+  duration: 2
+})
+
+gsap.to('.offer__lightnings', {
+  top: '-12%',
+  duration: 2
+})
+
+gsap.to('.offer__lines', {
+  rotate: 0,
+  duration: 2
+})
+
+gsap.to('.offer__service-items', {
+  opacity: 1,
+  duration: 2
+})
+
+gsap.to('.offer__service-item', {
+  duration: 2,
+  backdropFilter: 'blur(12px)'
+})
